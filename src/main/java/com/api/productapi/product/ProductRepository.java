0a495f5dfe6be3cab14bf5c44d0d8ce513b1cd23
JeyZ9 <111181111+JeyZ9@users.noreply.github.com>
2023-09-28 15:14:14 +0700

    Update ProductRepository.java

--- src/main/java/com/api/productapi/product/ProductRepository.java
+++ src/main/java/com/api/productapi/product/ProductRepository.java
@@ -6,9 +6,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository // เป็น Spring Data repository ใช้สำหรับจัดการและทำงานกับฐานข้อมูล
-// การสืบทอดจาก JpaRepository<Product, Long> ทำให้ ProductRepository มีความสามารถในการดำเนินการที่เกี่่ยวข้องกับข้อมูลผู้ใช้ในฐานข้อมูล และความสามารถที่จะทำงานร่วมกับข้อมูลผู้ใช้ที่มี primary key ประเภท Long
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    @Query("SELECT s FROM Product s WHERE s.id = ?1") // คือการกำหนดคำสั่ง JPQL ที่จะค้นหาข้อมูลผู้ใช้จากฐานข้อมูล
-    Optional<Product> findProductById(Long id); // Method นี้ใช้ค้นหาข้อมูลผู้ใช้จากฐานข้อมูลโดยใช้ ID เป็นเงื่อนไขการค้นหา มีพารามิเตอร์เดียวคือ ID และคืนผลลัพธ์ในรูปแบบของ Optional<Product>
-}
+    @Query("SELECT s FROM Product s WHERE s.id = ?1")
+    Optional<Product> findProductById(Long id);
